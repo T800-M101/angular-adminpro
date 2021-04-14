ebacc82dce6579e930c102f454263a841e327d34
Memo Moran <tuahil@gmail.com>
2021-04-14 17:06:02 -0600

    Fin seccion 4 - Router

---
+++ src/app/auth/auth.routing.ts
@@ -0,0 +1,19 @@
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from "./login/login.component";
+import { RegisterComponent } from "./register/register.component";
+
+
+const routes: Routes = [
+
+  //estas rutas usan el <router-oulet> principal
+{path: 'register', component:RegisterComponent},
+{path: 'login', component:LoginComponent},
+
+];
+
+@NgModule({
+  imports:[ RouterModule.forChild(routes) ],
+  exports:[ RouterModule]
+})
+export class AuthRoutingModule{}
